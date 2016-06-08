d0b3880577b3f50811ef7a2827a2e9480e781c62
Hristo Deshev <hristo@deshev.com>
2016-06-08 16:57:10 +0300

    Avoid sample test generation if test dir already exists.

--- postinstall.js
+++ postinstall.js
@@ -5,17 +5,23 @@ var projectDir = path.dirname(path.dirname(__dirname));
 var testsDir = path.join(projectDir, "e2e-tests");
 var packageJsonPath = path.join(projectDir, "package.json");
 
+var generateSampleTest = true;
+
 try {
     fs.mkdirSync(testsDir);
 } catch (e) {
     console.log("Test directory already exists: " + testsDir);
+    console.log("Skipping sample test generation.");
+    generateSampleTest = false;
 }
 
-var sampleTestSrc = path.join(__dirname, "sample-test.js");
-var sampleTestDest = path.join(testsDir, "sample-test.js");
-if (!fs.existsSync(sampleTestDest)) {
-    var javaClassesContent = fs.readFileSync(sampleTestSrc, "utf8");
-    fs.writeFileSync(sampleTestDest, javaClassesContent);
+if (generateSampleTest) {
+    var sampleTestSrc = path.join(__dirname, "sample-test.js");
+    var sampleTestDest = path.join(testsDir, "sample-test.js");
+    if (!fs.existsSync(sampleTestDest)) {
+        var javaClassesContent = fs.readFileSync(sampleTestSrc, "utf8");
+        fs.writeFileSync(sampleTestDest, javaClassesContent);
+    }
 }
 
 var packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
