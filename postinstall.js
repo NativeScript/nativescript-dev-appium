ee7696f542305fd79ee8571999f1ef3fb5429e5d
Marc Buils <marc.buils@unionup.fr>
2016-07-12 23:30:58 +0200

    [MBU] add features files

--- postinstall.js
+++ postinstall.js
@@ -2,27 +2,59 @@ var path = require("path");
 var fs = require("fs");
 
 var projectDir = path.dirname(path.dirname(__dirname));
-var testsDir = path.join(projectDir, "e2e-tests");
+var testsDirSrc = path.join(__dirname, "features");
+var testsDirDest = path.join(projectDir, "features");
+var stepDefinitionsSrc = path.join(testsDirSrc, "step_definitions");
+var stepDefinitionsDest = path.join(testsDirDest, "step_definitions");
+var supportSrc = path.join(testsDirSrc, "support");
+var supportDest = path.join(testsDirDest, "support");
 var packageJsonPath = path.join(projectDir, "package.json");
 
 var generateSampleTest = true;
 
 try {
-    fs.mkdirSync(testsDir);
+    fs.mkdirSync(testsDirDest);
+    fs.mkdirSync(stepDefinitionsDest);
+    fs.mkdirSync(supportDest);
 } catch (e) {
-    console.log("Test directory already exists: " + testsDir);
+    console.log("Test directory already exists: " + testsDirDest);
     console.log("Skipping sample test generation.");
     generateSampleTest = false;
 }
 
-/*if (generateSampleTest) {
-    var sampleTestSrc = path.join(__dirname, "sample-test.js");
-    var sampleTestDest = path.join(testsDir, "sample-test.js");
-    if (!fs.existsSync(sampleTestDest)) {
-        var javaClassesContent = fs.readFileSync(sampleTestSrc, "utf8");
-        fs.writeFileSync(sampleTestDest, javaClassesContent);
+if (generateSampleTest) {
+    var content;
+    var fileSrc;
+    var fileDest;
+    
+    fileSrc = path.join(testsDirSrc, "sample.feature");
+    fileDest = path.join(testsDirDest, "sample.feature");
+    if (!fs.existsSync(fileDest)) {
+        content = fs.readFileSync(fileSrc, "utf8");
+        fs.writeFileSync(fileDest, content);
     }
-}*/
+
+    fileSrc = path.join(stepDefinitionsSrc, "step_sample.js");
+    fileDest = path.join(stepDefinitionsDest, "step_sample.js");
+    if (!fs.existsSync(fileDest)) {
+        content = fs.readFileSync(fileSrc, "utf8");
+        fs.writeFileSync(fileDest, content);
+    }
+
+    fileSrc = path.join(supportSrc, "env.js");
+    fileDest = path.join(supportDest, "env.js");
+    if (!fs.existsSync(fileDest)) {
+        content = fs.readFileSync(fileSrc, "utf8");
+        fs.writeFileSync(fileDest, content);
+    }
+
+    fileSrc = path.join(supportSrc, "world.js");
+    fileDest = path.join(supportDest, "world.js");
+    if (!fs.existsSync(fileDest)) {
+        content = fs.readFileSync(fileSrc, "utf8");
+        fs.writeFileSync(fileDest, content);
+    }
+}
 
 var packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
