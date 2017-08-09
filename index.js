b5053b421aa20d9047a1d2597e31c015e465aba5
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-08-09 14:22:44 +0300

    Iclude js files for compatibilities

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require("./appium-setup");
-const glob = require("glob");
-const fs = require("fs");
-const path = require("path");
-const wd = require("wd");
-const yargs = require('yargs');
-const child_process = require("child_process");
-const utils = require("./utils");
-const elementFinder = require("./element-finder");
-
-const config = (() => {
-    const options = yargs
+var glob = require("glob");
+var fs = require("fs");
+var path = require("path");
+var wd = require("wd");
+var yargs = require('yargs');
+var child_process = require("child_process");
+var utils = require("./utils");
+var elementFinder = require("./element-finder");
+var server_options_1 = require("./server-options");
+var config = (function () {
+    var options = yargs
         .option("runType", { describe: "Path to excute command.", type: "string", default: null })
         .option("path", { alias: "p", describe: "Path to app root.", default: process.cwd, type: "string" })
         .option("testFolder", { describe: "E2e test folder name", default: "e2e", type: "string" })
@@ -18,137 +20,122 @@ const config = (() => {
         .option("verbose", { alias: "v", describe: "Log actions", default: false, type: "boolean" })
         .help()
         .argv;
-
-    const config = {
+    var config = {
         executionPath: options.path,
         loglevel: options.verbose,
         testFolder: options.testFolder,
         runType: options.runType || process.env.npm_config_runType,
         capsLocation: options.capsLocation || path.join(options.testFolder, "config"),
         isSauceLab: options.sauceLab
-    }
+    };
     return config;
 })();
-
-console.log("CONFIG: ", config);
-const {
-    executionPath,
-    loglevel,
-    testFolder,
-    runType,
-    capsLocation,
-    isSauceLab
-} = config;
-
-const appLocation = utils.appLocation;
-const appium = process.platform === "win32" ? "appium.cmd" : "appium";
-const projectDir = utils.projectDir();
-const pluginBinary = utils.pluginBinary();
-const projectBinary = utils.projectBinary();
-const pluginRoot = utils.pluginRoot();
-const pluginAppiumBinary = utils.resolve(pluginBinary, appium);
-const projectAppiumBinary = utils.resolve(projectBinary, appium);
-
-let caps;
-let customCapabilitiesConfigs;
-let customCapabilities;
-
+var executionPath = config.executionPath, loglevel = config.loglevel, testFolder = config.testFolder, runType = config.runType, capsLocation = config.capsLocation, isSauceLab = config.isSauceLab;
+var appLocation = utils.appLocation;
+var appium = process.platform === "win32" ? "appium.cmd" : "appium";
+var projectDir = utils.projectDir();
+var pluginBinary = utils.pluginBinary();
+var projectBinary = utils.projectBinary();
+var pluginRoot = utils.pluginRoot();
+var pluginAppiumBinary = utils.resolve(pluginBinary, appium);
+var projectAppiumBinary = utils.resolve(projectBinary, appium);
+var caps;
+var customCapabilitiesConfigs;
+var customCapabilities;
 try {
     customCapabilitiesConfigs = require("./capabilities-helper").searchCustomCapabilities(capsLocation);
     if (customCapabilitiesConfigs) {
         customCapabilities = JSON.parse(customCapabilitiesConfigs);
         utils.log(customCapabilities);
     }
-} catch (error) {
+}
+catch (error) {
     utils.logErr("No capabilities provided!!!");
 }
-
 if (fs.existsSync(pluginAppiumBinary)) {
     utils.log("Using plugin-local Appium binary.");
     appium = pluginAppiumBinary;
-} else if (fs.existsSync(projectAppiumBinary)) {
+}
+else if (fs.existsSync(projectAppiumBinary)) {
     utils.log("Using project-local Appium binary.");
     appium = projectAppiumBinary;
-} else {
+}
+else {
     utils.log("Using global Appium binary.");
 }
-
-let server;
-let serverPort = 9200;
-exports.startAppiumServer = function (port) {
-    serverPort = port || serverPort;
+var server;
+var serverOptoins = new server_options_1.ServerOptions(9200);
+function startAppiumServer(port) {
+    serverOptoins.port = port || serverOptoins.port;
     server = child_process.spawn(appium, ["-p", port], {
         shell: true,
         detached: false
     });
-
     return utils.waitForOutput(server, /listener started/, 60000);
 }
-
-exports.killAppiumServer = () => {
+exports.startAppiumServer = startAppiumServer;
+function killAppiumServer() {
     // todo: check if allready dead?
     var isAlive = true;
     if (isAlive) {
-        return new Promise((resolve, reject) => {
-            server.on("close", (code, signal) => {
-                console.log(`Appium terminated due ${signal}`);
+        return new Promise(function (resolve, reject) {
+            server.on("close", function (code, signal) {
+                console.log("Appium terminated due " + signal);
                 resolve();
             });
             // TODO: What about "error".
             server.kill('SIGINT');
             server = null;
         });
-    } else {
+    }
+    else {
         return Promise.resolve();
     }
 }
-
-exports.createDriver = (capabilities, activityName) => {
+exports.killAppiumServer = killAppiumServer;
+function createDriver(capabilities, activityName) {
     console.log("Creating driver");
     caps = capabilities;
     if (!activityName) {
         activityName = "com.tns.NativeScriptActivity";
     }
-
     if (!caps) {
         caps = customCapabilities[runType];
         if (!caps) {
             throw new Error("Incorrect test run type: " + runType + " . Available run types are :" + customCapabilitiesConfigs);
         }
     }
-
-    let config = {
+    var config = {
         host: "localhost",
-        port: serverPort
+        port: serverOptoins.port
     };
-
     if (isSauceLab) {
-        const sauceUser = process.env.SAUCE_USER;
-        const sauceKey = process.env.SAUCE_KEY;
-
+        var sauceUser = process.env.SAUCE_USER;
+        var sauceKey = process.env.SAUCE_KEY;
         if (!sauceKey || !sauceUser) {
             throw new Error("Sauce Labs Username or Access Key is missing! Check environment variables for SAUCE_USER and SAUCE_KEY !!!");
         }
-
-        config = "https://" + sauceUser + ":" + sauceKey + "@ondemand.saucelabs.com:443/wd/hub";
+        // TODO: Should be tested
+        config = {
+            host: "https://" + sauceUser + ":" + sauceKey + "@ondemand.saucelabs.com:443/wd/hub",
+            port: 0
+        };
     }
-
-    const driver = wd.promiseChainRemote(config);
-    exports.configureLogging(driver);
-
+    var driver = wd.promiseChainRemote(config);
+    configureLogging(driver);
     if (appLocation) {
         caps.app = isSauceLab ? "sauce-storage:" + appLocation : appLocation;
-    } else if (!caps.app) {
+    }
+    else if (!caps.app) {
         console.log("Getting caps.app!");
         caps.app = getAppPath();
     }
-
     console.log("Creating driver!");
     return driver.init(caps);
-};
-
-
-exports.configureLogging = (driver) => {
+}
+exports.createDriver = createDriver;
+;
+function configureLogging(driver) {
     driver.on("status", function (info) {
         utils.log(info.cyan);
     });
@@ -158,33 +145,38 @@ exports.configureLogging = (driver) => {
     driver.on("http", function (meth, path, data) {
         utils.log(" > " + meth.magenta + path + " " + (data || "").grey);
     });
-};
-
-exports.getXPathWithExactText = (text) => {
+}
+exports.configureLogging = configureLogging;
+;
+function getXPathWithExactText(text) {
     return elementFinder.getXPathByText(text, true, runType);
 }
-
-exports.getXPathContainingsText = (text) => {
+exports.getXPathWithExactText = getXPathWithExactText;
+function getXPathContainingsText(text) {
     return elementFinder.getXPathByText(text, false, runType);
 }
-
-exports.getElementClass = (name) => {
+exports.getXPathContainingsText = getXPathContainingsText;
+function getElementClass(name) {
     return elementFinder.getElementClass(name, runType);
 }
-
+exports.getElementClass = getElementClass;
 function getAppPath() {
     console.log("runType " + runType);
     if (runType.includes("android")) {
-        const apks = glob.sync("platforms/android/build/outputs/apk/*.apk").filter(function (file) { return file.indexOf("unaligned") < 0; });
+        var apks = glob.sync("platforms/android/build/outputs/apk/*.apk").filter(function (file) { return file.indexOf("unaligned") < 0; });
         return apks[0];
-    } else if (runType.includes("ios-simulator")) {
-        const simulatorApps = glob.sync("platforms/ios/build/emulator/**/*.app");
+    }
+    else if (runType.includes("ios-simulator")) {
+        var simulatorApps = glob.sync("platforms/ios/build/emulator/**/*.app");
         return simulatorApps[0];
-    } else if (runType.includes("ios-device")) {
-        const deviceApps = glob.sync("platforms/ios/build/device/**/*.ipa");
+    }
+    else if (runType.includes("ios-device")) {
+        var deviceApps = glob.sync("platforms/ios/build/device/**/*.ipa");
         return deviceApps[0];
-    } else {
+    }
+    else {
         throw new Error("No 'app' capability provided and incorrect 'runType' convention used: " + runType +
             ". In order to automatically search and locate app package please use 'android','ios-device','ios-simulator' in your 'runType' option. E.g --runType=android23, --runType=ios-simulator10iPhone6");
     }
-};
+}
+;
---
+++ server-options.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ServerOptions = (function () {
+    function ServerOptions(_port) {
+        this._port = _port;
+    }
+    Object.defineProperty(ServerOptions.prototype, "port", {
+        get: function () {
+            return this._port;
+        },
+        set: function (port) {
+            this._port = port;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return ServerOptions;
+}());
+exports.ServerOptions = ServerOptions;
