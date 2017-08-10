878eaa8bf7cfbec5b86d701076ae68209c0be00d
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-08-11 00:19:59 +0300

    Remove appium setup

--- appium-setup-test.ts
+++
@@ -1,8 +0,0 @@
-var wd = require("wd");
-require('colors');
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-export var should = chai.should();
-
-chaiAsPromised.transferPromiseness = wd.transferPromiseness;
