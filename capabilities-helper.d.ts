8f72d13649a7dabea6d1536cbb899b16431b4c6d
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-08-12 21:55:52 +0300

    Update d.ts

--- capabilities-helper.d.ts
+++ capabilities-helper.d.ts
@@ -1,1 +1,3 @@
-export declare function searchCustomCapabilities(capabilitiesLocation: any): {};
+/// <reference types="node" />
+export declare function resolveCapabilities(capsLocation: string, runType: string): {};
+export declare function searchCustomCapabilities(capabilitiesLocation: any): Buffer;
