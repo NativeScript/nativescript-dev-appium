fb2123f56a96c40b4385cb9ccf73d0bfb38b8390
vchimev <Vasil.Chimev@telerik.com>
2017-10-06 09:48:40 +0300

    refactor: delete an unnecessary file

--- lib/emulators-manager.d.ts
+++
@@ -1,9 +0,0 @@
-import { INsCapabilities } from "./ins-capabilities";
-export declare class EmulatorManager {
-    private _args;
-    private verbose;
-    private _emulator;
-    constructor(_args: INsCapabilities, verbose?: boolean);
-    startEmulator(name: any): Promise<boolean>;
-    stop(): Promise<{}>;
-}
