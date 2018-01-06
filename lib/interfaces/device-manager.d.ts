e5ea780ec1ee071d790f06ccdff81d115505b54f
Svetoslav <tsenov.svetoslav@gmail.com>
2018-01-07 00:46:05 +0200

    chore: fix typings (#80)

--- lib/interfaces/device-manager.d.ts
+++ lib/interfaces/device-manager.d.ts
@@ -1,6 +1,6 @@
 import { INsCapabilities } from "../interfaces/ns-capabilities";
 import { IDevice } from "mobile-devices-controller";
 export interface IDeviceManager {
-    startDevice(args: INsCapabilities): IDevice;
-    stopDevice(args: INsCapabilities): IDevice;
+    startDevice(args: INsCapabilities): Promise<IDevice>;
+    stopDevice(args: INsCapabilities): Promise<IDevice>;
 }
