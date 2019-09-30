4447e5c6b780c0e988d068bba3f91c5d3ad5b936
Zdravko Branzov <Zdravko.Branzov@progress.com>
2019-09-30 17:39:41 +0300

    chore: create multiple actual images based on LogImageType

--- lib/image-helper.ts
+++ lib/image-helper.ts
@@ -268,9 +268,13 @@ export class ImageHelper {
         // Iterate
         if (!result) {
             const eventStartTime = Date.now().valueOf();
+            let counter = 1;
             options.timeOutSeconds *= 1000;
+            let pathActualImageCounter = resolvePath(this._args.reportsPath, imageName.replace(".", "_actual."));
             while ((Date.now().valueOf() - eventStartTime) <= options.timeOutSeconds && !result) {
-                const pathActualImageCounter = resolvePath(this._args.reportsPath, imageName.replace(".", "_actual."));
+                if (checkImageLogType(this._args.testReporter, LogImageType.everyImage)) {
+                    pathActualImageCounter = resolvePath(this._args.reportsPath, imageName.replace(".", "_actual_" + counter + "."));
+                }
                 pathActualImage = await this._driver.saveScreenshot(pathActualImageCounter);
                 if (!options.keepOriginalImageSize) {
                     await this.clipRectangleImage(options.cropRectangle, pathActualImage);
@@ -278,9 +282,10 @@ export class ImageHelper {
                 // await this.prepareImageToCompare(pathActualImage, this.imageCropRect);
                 result = await this.compareImages(options, pathActualImage, pathExpectedImage, pathDiffImage);
                 if (!result && checkImageLogType(this._args.testReporter, LogImageType.everyImage)) {
-                    this._args.testReporterLog(`Actual image: ${basename(pathActualImage)}`);
+                    this._args.testReporterLog(`Actual image: ${basename(pathActualImage).replace(/\.\w{3,3}$/ig, "")}`);
                     this._args.testReporterLog(join(this._args.reportsPath, basename(pathActualImage)));
                 }
+                counter++;
             }
 
             if (options.overwriteActualImage === true && !result) {
